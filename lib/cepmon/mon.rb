dcb203611c1b73a295a9d7533655ad76eac058dd
Pete Fritchman <petef@databits.net>
2011-06-22 17:37:56 -0700

    initial stab at amqp -> esper stream, needs config-izing

---
+++ lib/cepmon/mon.rb
@@ -0,0 +1,29 @@
+require "cepmon/engine"
+require "cepmon/eventlistener"
+require "cepmon/metric"
+require "rubygems"
+require "bunny"
+
+module CEPMon
+  class Mon
+    def initialize(config)
+      @config = config
+      @engine = CEPMon::Engine.new
+      @event_listener = CEPMon::EventListener.new(@engine, true)
+      @engine.add_statements(@config, @event_listener)
+    end
+
+    def run(args)
+      amqp = Bunny.new(:user => "guest", :pass => "guest")
+      amqp.start
+      queue = amqp.queue("cepmon-#{Process.pid}", :auto_delete => true)
+      exchange = amqp.exchange("stats", :type => :topic, :durable => true)
+      queue.bind(exchange)
+      queue.subscribe do |msg|
+        msg[:payload].split("\n").each do |line|
+          CEPMon::Metric.new(line).send(@engine)
+        end
+      end
+    end # def run
+  end # class Mon
+end # module CEPMon
