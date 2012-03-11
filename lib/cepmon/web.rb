d9a5ac5ec174699699cd6e66fabeff76e61c5e7b
Pete Fritchman <petef@databits.net>
2012-03-11 10:22:54 -0700

    add alert filtering by url param, and rule link handlers/helpers

--- lib/cepmon/web.rb
+++ lib/cepmon/web.rb
@@ -2,6 +2,7 @@ require "rubygems"
 require "cgi"
 require "sinatra/base"
 require "thread"
+require "uri"
 
 class CEPMon
   class Web < Sinatra::Base
@@ -37,15 +38,44 @@ class CEPMon
       def h(obj)
         CGI::escapeHTML(obj.to_s)
       end
+
+      def rule_link(name, text)
+        "<a href=\"/rules/#{name}\">#{h(text)}</a>"
+      end
+
+      def filter_alerts(alerts, params)
+        res = alerts
+        if params[:statement]
+          res.delete_if { |a| a.statement != params[:statement] }
+        end
+        if params[:cluster]
+          res.delete_if { |a| a.cluster != params[:cluster] }
+        end
+        if params[:host]
+          res.delete_if { |a| a.host != params[:host] }
+        end
+        return res
+      end
+    end
+
+    get "/rules" do
+      @rules = @@event_listener.engine.statements
+      erb :rules
+    end
+
+    get "/rules/:rule" do
+      @rule = @@event_listener.engine.statements.select { |r| r[:statement] == params[:rule] }.first
+      raise "can't find rule #{params[:rule]}" unless @rule
+      erb :rule_detail
     end
 
     get "/history" do
-      @alerts = @@event_listener.history
+      @alerts = filter_alerts(@@event_listener.history, params)
       erb :history
     end
 
     get "/alerts" do
-      @alerts = @@event_listener.alerts
+      @alerts = filter_alerts(@@event_listener.alerts, params)
       erb :alerts
     end
   end # class Web
